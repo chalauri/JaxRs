3dff66263042617b2f7e80ee1b7cde79df99fb5d
Giga <giga.chalauri@gmail.com>
2015-01-11 13:04:12 +0400

    new

--- src/configuration/Config.java
+++ src/configuration/Config.java
@@ -1,9 +1,24 @@
 package configuration;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
+
+import main.java.HelloResource;
 
 @ApplicationPath("/resources")
 public class Config extends Application {
 
+	private Set<Object> singletons = new HashSet<Object>();
+
+	public Config() {
+		singletons.add(new HelloResource());
+	}
+
+	@Override
+	public Set<Object> getSingletons() {
+		return singletons;
+	}
 }
