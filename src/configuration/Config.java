cc95c6a953290b853311a096da8598318a0d0f50
Giga <giga.chalauri@gmail.com>
2015-01-11 13:21:17 +0400

    new

--- src/configuration/Config.java
+++ src/configuration/Config.java
@@ -5,15 +5,19 @@ import java.util.Set;
 
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
+import javax.ws.rs.ext.Provider;
 
 import main.java.HelloResource;
 
 @ApplicationPath("/resources")
+@Provider
 public class Config extends Application {
 
 	private Set<Object> singletons = new HashSet<Object>();
+	private Set<Class<?>> empty = new HashSet<Class<?>>();
 
 	public Config() {
+		System.out.println("\n\n\n\n\n\n\n\n\n\n CHEMI KAI");
 		singletons.add(new HelloResource());
 	}
 
@@ -21,4 +25,10 @@ public class Config extends Application {
 	public Set<Object> getSingletons() {
 		return singletons;
 	}
+
+	@Override
+	public Set<Class<?>> getClasses() {
+		return empty;
+	}
+
 }
--- src/main/java/HelloResource.java
+++ src/main/java/HelloResource.java
@@ -4,12 +4,11 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 
-
-
+@Path("/hello")
 public class HelloResource {
 	
 	@GET
-	@Path("/hello")
+	@Path("/sayHello")
 	@Produces("text/plain")
 	public String sayHello(){
 		
